88f2e90ce066adadb95c19b53426f7a37ea3a3ee
Tanmay <jaintanmay389@gmail.com>
2021-12-14 11:41:17 +0530

    final commit

--- src/app/services/authService/auth-service.service.ts
+++ src/app/services/authService/auth-service.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
-import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
